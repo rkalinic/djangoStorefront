062eed50cdf6338acec18368bdcbc044fda92654
Robert <robertkalinic19@gmail.com>
2024-08-11 17:17:55 +0200

    apps store and tags

---
+++ store/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_CHOICES =  [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices = MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+class Order(models.Model):
+    placed_at = models.DateTimeField(auto_now_add=True)
+    PENDING = 'P'
+    COMPLETE = 'C'
+    FAILED = 'F'
+    PAYMENT_STATUS = [
+        (PENDING, 'Pending'),
+        (COMPLETE, 'Complete'),
+        (FAILED, 'Failed')
+    ] 
+    payment_status = models.CharField(max_length=1, choices = PAYMENT_STATUS, default=PENDING)
